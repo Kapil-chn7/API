2e464521232f4561ab6ed5a5e7642d5db86888ae
Kapil Chauhan <kapilchn7@gmail.com>
2023-09-07 10:02:40 +0530

    Changes in index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import Fastify from "fastify";
-import swagger from "@fastify/swagger";
-import { withRefResolver } from "fastify-zod";
-// import { serve, setup } from "swagger-ui-fastify";
 import Routes from "./routes/routerpost.js";
 import prismaPlugin from "./database/prisma_connection.js";
 
@@ -13,47 +10,6 @@ fastify.register(prismaPlugin);
 
 //registrations
 fastify.register(Routes);
-
-fastify.register(swagger, {
-  swagger: {
-    info: {
-      title: "Test swagger",
-      description: "Testing the Fastify swagger API",
-      version: "0.1.0",
-    },
-    externalDocs: {
-      url: "https://swagger.io",
-      description: "Find more info here",
-    },
-    host: "localhost",
-    schemes: ["http"],
-    consumes: ["application/json"],
-    produces: ["application/json"],
-    tags: [
-      { name: "user", description: "User related end-points" },
-      { name: "code", description: "Code related end-points" },
-    ],
-    definitions: {
-      User: {
-        type: "object",
-        required: ["id", "email"],
-        properties: {
-          id: { type: "string", format: "uuid" },
-          firstName: { type: "string" },
-          lastName: { type: "string" },
-          email: { type: "string", format: "email" },
-        },
-      },
-    },
-    securityDefinitions: {
-      apiKey: {
-        type: "apiKey",
-        name: "apiKey",
-        in: "header",
-      },
-    },
-  },
-});
 
 const start = async () => {
   try {
